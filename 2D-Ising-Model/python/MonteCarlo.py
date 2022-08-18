70fe43cf5114dfe0a90ddbf2d1ccebe2787d6b80
JYNi16 <49049752+JYNi16@users.noreply.github.com>
2022-08-18 22:26:41 +0800

    multiprocessing for MC

--- 2D-Ising-Model/python/MonteCarlo.py
+++ 2D-Ising-Model/python/MonteCarlo.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from multiprocessing import Pool 
 
 class MonteCarlo():
     
@@ -26,48 +27,63 @@ class MonteCarlo():
         
         return delta_E 
     
-    def main_loop(self):
+    def main_loop(self, T):
         
         S = self.Lattice ** 2
-        f_M = open('M.dat','w')
-        f_C = open('C.dat','w')
+        #f_M = open('M.dat','w')
+        #f_C = open('C.dat','w')
             
-        for T in np.arange(0.1, 2.0, 0.05):
-            spin = np.ones([self.Lattice, self.Lattice], dtype = int)
-            C = 0
-            C_heat = []
-            mag = []
-            mag_2 = []
-            for sweep in range(self.sweeps + self.relax):
-                for i in range(self.Lattice):
-                    for j in range(self.Lattice):
-                        delta_E = self.cal_delta_E(spin, i, j)
-                        print("np.exp(delta_E) is:", np.exp((-delta_E)/T))
-                        if delta_E <= 0:
-                            spin[i,j] = -spin[i,j]
+        #for T in np.arange(0.1, 2.0, 0.05):
+        t = T/10 
+        spin = np.ones([self.Lattice, self.Lattice], dtype = int)
+        C = 0
+        C_heat = []
+        mag = []
+        mag_2 = []
+        for sweep in range(self.sweeps + self.relax):
+            for i in range(self.Lattice):
+                for j in range(self.Lattice):
+                    delta_E = self.cal_delta_E(spin, i, j)
+                        #print("np.exp(delta_E) is:", np.exp((-delta_E)/T))
+                    if delta_E <= 0:
+                        spin[i,j] = -spin[i,j]
                         # 几率翻转
-                        else: 
-                            if np.exp((-delta_E)/T) > np.random.random():
-                                spin[i,j] = - spin[i,j]
+                    else: 
+                        if np.exp((-delta_E)/t) > np.random.random():
+                            spin[i,j] = - spin[i,j]
                                 
-                M = abs(sum(sum(spin))) / S   
-                M_2 = M ** 2 
-                mag_2.append(M_2)
-                mag.append(M)
+            M = abs(sum(sum(spin))) / S   
+            M_2 = M ** 2 
+            mag_2.append(M_2)
+            mag.append(M)
             
-            result = sum(mag[self.relax:]) / self.sweeps
-            print("result is:", spin)
-            result_2 = sum(mag_2[self.relax:]) / self.sweeps 
-            result_C = (result_2 - result ** 2) / T # 求磁比热
-             
-            f_M.write(str(T)+'  '+str(result)+"\n") # 
-            f_C.write(str(T)+'  '+str(result_C)+"\n")
+        result = sum(mag[self.relax:]) / self.sweeps
+        test = [] 
+        test.append(t)
+        test.append(result)
+        #print("result is:", spin)
+        result_2 = sum(mag_2[self.relax:]) / self.sweeps 
+        result_C = (result_2 - result ** 2) / T # 求磁比热
         
-        f_M.close()
-        f_C.close()
+        np.save("M_T/{:d}.npy".format(T), test)
+             
+            #f_M.write(str(T)+'  '+str(result)+"\n") # 
+            #f_C.write(str(T)+'  '+str(result_C)+"\n")
+        
+        #f_M.close()
+        #f_C.close()
+    def multi_run(self):
+        
+        ing_argv = [] 
+        for T in range(3, 30, 1):
+            ing_argv.append(T)
+        
+        p = Pool(8)
+        p.map(self.main_loop, ing_argv)
+        
     
     
 
 if __name__=="__main__":
     MC_model = MonteCarlo(100, 5000, 1, 1, 0, 10)
-    MC_model.main_loop() 
+    MC_model.multi_run()
--- 2D-Ising-Model/python/plot.py
+++ 2D-Ising-Model/python/plot.py
@@ -2,26 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math  
 from scipy.optimize import curve_fit
+import os, glob 
 
-p = math.pi
-T = [] 
-M = [] 
-C = []
-i = 0 
+def load_MT(root):
+    
+    path_list = []
+    path_list.extend(glob.glob(os.path.join(root, "*.npy")))
+    
+    M, T = [], []
+    path_list.sort(key=len)
+    
+    for i in range(len(path_list)):
+        data = np.load(path_list[i])
+        print(data)
+        M.append(data[1])
+        T.append(data[0])
+    
+    return T, M 
 
-with open ("M.dat",'r') as f1:
-    while( i < 50):
-        i = i + 1
-        value = f1.readline()
-        v = [float(s) for s in value.split()]
-        T.append(v[0])
-        M.append(v[1])
-        C.append(v[2])
+def plot(T, M):
+    plt.figure(figsize = (7,5))
+    #p1 = plt.subplot(121)
+    plt.plot(T,M,'o-', color = "#FF0000", markersize = 8, linewidth =1, label = r'$M$')
+    #p1 = plt.subplot(122)
+    #p1.plot(T,C,'o-', color = "#000000", markersize = 8, linewidth =1, label = r'$M$')
+    plt.show() 
 
 
-plt.figure(figsize = (16,5))
-p1 = plt.subplot(121)
-p1.plot(T,M,'o-', color = "#FF0000", markersize = 8, linewidth =1, label = r'$M$')
-p1 = plt.subplot(122)
-p1.plot(T,C,'o-', color = "#000000", markersize = 8, linewidth =1, label = r'$M$')
-plt.show() 
+if __name__=="__main__":
+    T, M = load_MT("E:/MonteCarlo/2D-Ising-Model/python/M_T")
+    plot(T, M)
+    
+        
+    
+    
+
+
