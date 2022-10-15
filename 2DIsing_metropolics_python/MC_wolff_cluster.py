80d780873df5027c701c83d3a8df1cbf6377c91f
JYNi16 <49049752+JYNi16@users.noreply.github.com>
2022-10-15 17:24:35 +0800

    Add files via upload

--- 2DIsing_metropolics_python/MC_wolff_cluster.py
+++ 2DIsing_metropolics_python/MC_wolff_cluster.py
@@ -4,42 +4,29 @@ from multiprocessing import Pool
 from numba import jit
 import time
 
-sweeps = 500
+sweeps = 600
 K = 1
 J = 1
 H = 0
-Lattice = 40
-relax = 100
-
-
-def left(x, y):
-    if x < 0.5: return [Lattice - 1, y]
-    else: return [x - 1, y]
-
-def right(x, y):
-    if x > Lattice - 1.5: return [0, y]
-    else: return [x + 1, y]
-
-def up(x, y):
-    if y < 0.5: return [x, Lattice - 1]
-    else: return [x, y - 1]
+Lattice = 80
+relax = 200
 
 def down(x, y):
     if y > Lattice - 1.5: return [x, 0]
     else: return [x, y + 1]
 
-#@jit
-def wolff_cluster(spin, t):
+@jit
+def wolff_cluster(spin, P_add):
     mag = []
     mag_2 = []
+
     for k in range(sweeps + relax):
         x = np.random.randint(0, Lattice)
         y = np.random.randint(0, Lattice)
         sign = spin[x, y]
-        P_add = 1 - np.exp(-2 * J / t)
         stack = [[x, y]]
-        lable = np.ones([Lattice, Lattice], int)
-        lable[x, y] = 0
+        lable = [[1 for i in range(Lattice)] for j in range(Lattice)]
+        lable[x][y] = 0
 
         while len(stack) > 0.5:
 
@@ -50,36 +37,44 @@ def wolff_cluster(spin, t):
             # Append neighbor spins
 
             # Left neighbor
+            if x_site < 0.5:
+                [leftx, lefty] = [Lattice - 1, y_site]
+            else:
+                [leftx, lefty] = [x_site - 1, y_site]
 
-            [leftx, lefty] = left(x_site, y_site)
-
-            if spin[leftx, lefty] * sign > 0.5  and np.random.rand() < P_add:
+            if spin[leftx, lefty] * sign > 0.5 and np.random.rand() < P_add:
                 stack.append([leftx, lefty])
-                lable[leftx, lefty] = 0
+                lable[leftx][lefty] = 0
 
             # Right neighbor
-
-            [rightx, righty] = right(x_site, y_site)
+            if x_site > Lattice - 1.5:
+                [rightx, righty] = [0, y_site]
+            else:
+                [rightx, righty] = [x_site + 1, y_site]
 
             if spin[rightx, righty] * sign > 0.5 and np.random.rand() < P_add:
                 stack.append([rightx, righty])
-                lable[rightx, righty] = 0
+                lable[rightx][righty] = 0
 
             # Up neighbor
-
-            [upx, upy] = up(x_site, y_site)
+            if y_site < 0.5:
+                [upx, upy] = [x_site, Lattice - 1]
+            else:
+                [upx, upy] = [x_site, y_site - 1]
 
             if spin[upx, upy] * sign > 0.5 and np.random.rand() < P_add:
                 stack.append([upx, upy])
-                lable[upx, upy] = 0
+                lable[upx][upy] = 0
 
             # Down neighbor
+            if y_site > Lattice - 1.5:
+                [downx, downy] = [x_site, 0]
+            else:
+                [downx, downy] = [x_site, y_site + 1]
 
-            [downx, downy] = down(x_site, y_site)
-
-            if spin[downx, downy] * sign > 0.5 and  np.random.rand() < P_add:
+            if spin[downx, downy] * sign > 0.5 and np.random.rand() < P_add:
                 stack.append([downx, downy])
-                lable[downx, downy] = 0
+                lable[downx][downy] = 0
 
         if k > relax:
             M = np.abs(np.mean(spin))
@@ -92,10 +87,10 @@ def wolff_cluster(spin, t):
 
 def main_loop(T):
     t = T / 10
-    spin = np.ones([Lattice, Lattice], dtype=int)
-
-    mag, mag_2 = wolff_cluster(spin, t)
-
+    P_add = 1 - np.exp(-2 * J / t)
+    spin = np.random.randint(-1, 1, (Lattice, Lattice))
+    spin[spin == 0] = 1
+    mag, mag_2 = wolff_cluster(spin, P_add)
     result = np.mean(mag)
     print("t is", t, "result is:", result)
     test = []
@@ -112,7 +107,7 @@ def main_loop(T):
 
 def single_run():
     start = time.time()
-    for T in range(3, 30, 1):
+    for T in range(6, 40, 1):
         main_loop(T)
 
     end = time.time()
@@ -125,7 +120,7 @@ def multi_run():
     ing_argv = []
     for T in range(8, 40, 1):
         ing_argv.append(T)
-    with Pool(16) as p:
+    with Pool(8) as p:
         p.map(main_loop, ing_argv)
 
     end = time.time()
@@ -134,4 +129,4 @@ def multi_run():
 
 
 if __name__ == "__main__":
-    single_run()
+    multi_run()
